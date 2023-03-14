6802142c501b0a2e2d8e6ead59f972a7e7d3c9bf
Oliver Rutsch <orutsch@mailbox.org>
2023-03-14 20:33:16 +0100

    Weiterleitung an Neelix

--- raspi/MQTTInfluxDBBridge.py
+++ raspi/MQTTInfluxDBBridge.py
@@ -7,7 +7,10 @@ import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 import time
 import logging
-
+import urllib3
+urllib3.disable_warnings()
+logging.basicConfig(format=FORMAT, filename='/var/log/MQTTInfluxBridge.log', encoding='utf-8', level=logging.INFO)
+#logging.basicConfig(format='%(asctime)s %(message)s', encoding='utf-8', level=logging.INFO)
 INFLUXDB_ADDRESS = 'localhost'
 INFLUXDB_USER = 'mqtt'
 INFLUXDB_PASSWORD = 'mqtt'
@@ -36,6 +39,8 @@ TopicVBatt_gwhs="/wetterstation/gwhs/vbatt"
 TopicCounter=0
 
 influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
+# Virtueller Rechner im Netz
+NeelixClient = InfluxDBClient(host='neelix.ddnss.de', port=8086, username='mqtt', password='4m0a21gvc', ssl=True, verify_ssl=False)
 
 class SensorData(NamedTuple):
     location: str
@@ -65,27 +70,33 @@ def _parse_mqtt_message(topic, payload):
         measurement = match.group(2)
         if measurement == 'status':
             return None
+        if payload == '':
+            return None
         return SensorData(location, measurement, float(payload))
     else:
         print('Kein match:'+topic)
         return None
 
 def _send_sensor_data_to_influxdb(sensor_data):
-    json_body = [
-        {
-            'measurement': sensor_data.measurement,
-            'tags': {
-                'location': sensor_data.location
-            },
-            'fields': {
-                'value': sensor_data.value
+    try:
+        json_body = [
+            {
+                'measurement': sensor_data.measurement,
+                'tags': {
+                    'location': sensor_data.location
+                },
+                'fields': {
+                    'value': sensor_data.value
+                }
             }
-        }
-    ]
-    print('JSON: '+str(json_body))
-    influxdb_client.write_points(json_body)
-
+        ]
+        print('JSON: '+str(json_body))
+        influxdb_client.write_points(json_body)
+        NeelixClient.write_points(json_body)
+    except Exception as e:
+        logging.info("Fehler", e.__class__, "occurred: ",e)
 def _send_error_log_to_influxdb(error_log):
+
     json_body = [
         {
             'errorlog': error_log
@@ -93,33 +104,38 @@ def _send_error_log_to_influxdb(error_log):
     ]
     print('JSON: '+str(json_body))
     influxdb_client.write_points(json_body)
-
+    NeelixCient.write_points(json_body)
+    
 def on_message(client, userdata, msg):
     """The callback for when a PUBLISH message is received from the server."""
     print(msg.topic + ' ' + str(msg.payload))
-    try:
-        sensor_data = _parse_mqtt_message(msg.topic, msg.payload.decode('utf-8'))
-        if sensor_data is not None:
-            _send_sensor_data_to_influxdb(sensor_data)
-        else:
-            if msg.topic is TopicError:
-                status=msg.payload.decode('utf-8')
-                _send_error_log_to_influxdb(status)
-                    
-    except Exception as e:
-        logging.info("Fehler", e.__class__, "occurred: ",e)
-        
+  
+    sensor_data = _parse_mqtt_message(msg.topic, msg.payload.decode('utf-8'))
+    if sensor_data is not None:
+        _send_sensor_data_to_influxdb(sensor_data)
+    else:
+        if msg.topic is TopicError:
+            status=msg.payload.decode('utf-8')
+            _send_error_log_to_influxdb(status)     
     
-
 def _init_influxdb_database():
-    databases = influxdb_client.get_list_database()
-    if len(list(filter(lambda x: x['name'] == INFLUXDB_DATABASE, databases))) == 0:
-        influxdb_client.create_database(INFLUXDB_DATABASE)
-    influxdb_client.switch_database(INFLUXDB_DATABASE)
-
+    try:
+        databases = influxdb_client.get_list_database()
+        print(databases)
+        if len(list(filter(lambda x: x['name'] == INFLUXDB_DATABASE, databases))) == 0:
+            influxdb_client.create_database(INFLUXDB_DATABASE)
+        influxdb_client.switch_database(INFLUXDB_DATABASE)
+        
+        databases = NeelixClient.get_list_database()
+        print(databases)
+        if len(list(filter(lambda x: x['name'] == INFLUXDB_DATABASE, databases))) == 0:
+            NeelixClient.create_database(INFLUXDB_DATABASE)
+        NeelixClient.switch_database(INFLUXDB_DATABASE)
+    except Exception as e:
+        logging.info("Fehler", e.__class__, "occurred: ",e)    
 def main():
-    print("Warte 60 s auf den Start der Datenbank...")
-    time.sleep(60)
+    print("Warte 20 s auf den Start der Datenbank...")
+    time.sleep(20)
     _init_influxdb_database()
 
     mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
